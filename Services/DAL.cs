b48e83cf839f9e0edf1d13a1993b84f4eac1566f
Rick Vickery <rickvickery@quickenloans.com>
2020-10-19 17:29:36 -0400

    Added the IDAL, DAL, some of the DAL methods

--- Services/DAL.cs
+++ Services/DAL.cs
@@ -1,11 +1,58 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using UpmeetProject.Models;
 
 namespace UpmeetProject.Services
 {
-    public class DAL
+
+
+    public class DAL : IDAL
     {
+
+        private string connString;
+        public DAL(IConfiguration config)
+        {
+            connString = config.GetConnectionString("rick");
+        }
+
+        public Event AddEvent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int AddFavorite(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Event GetEvent(int id)
+        {
+
+            SqlConnection conn = new SqlConnection(connString);
+            string command = "SELECT * FROM Events WHERE ID=@id";
+            Event result = (Event)conn.Query<Event>(command, new { id });
+            return result;
+
+        }
+
+        public Favorite GetFavorite(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveFavorite(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UpmeetProject.Services;
 
 namespace UpmeetProject
 {
@@ -26,6 +27,8 @@ namespace UpmeetProject
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddTransient<IDAL, DAL>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
