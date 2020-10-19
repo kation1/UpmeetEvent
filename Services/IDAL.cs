0c3eab5d9b69c6308af54edf1107ee4c1aa8d069
Rick Vickery <rickvickery@quickenloans.com>
2020-10-19 17:09:53 -0400

    Added the IDAL

---
+++ Services/IDAL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UpmeetProject.Models;
+
+namespace UpmeetProject.Services
+{
+    public interface IDAL
+    {
+
+        IEnumerable<string> GetList();
+        //Maybe a class?
+
+        Event AddEvent();
+
+        Event GetEvent(int id);
+        //Maybe a long?
+
+        Favorite GetFavorite(int id);
+
+        int AddFavorite(int id);
+
+        int RemoveFavorite(int id);
+
+
+
+    }
+}
